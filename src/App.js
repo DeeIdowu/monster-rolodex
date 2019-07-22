fe5b46df03884d964327d3d1db16bb9b0b3b23f5
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-07-22 19:56:11 -0400

    newjug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //example of using state in class component:
+  constructor() {
+    super(); //responsible for calling constructor method on component class, enabling
+    //access to state
+    this.state = {
+      monsters: [
+        {
+          name: "Wilbert",
+          id: "W"
+        },
+        {
+          name: "Albert",
+          id: "A"
+        },
+        {
+          name: "Bobert",
+          id: "B"
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {//to render the names of monster in h1 tag via map, map is an iteration of the elements in the array
+        this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
