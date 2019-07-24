54598c563a3dbf7224b06d5e21af29a6c34791f3
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2019-07-23 21:12:51 -0400

    Updates

---
+++ precache-manifest.6ee2a179e16e40c12e9f76b318b3e404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038da8c48159e87e5bdf0623ffa2e5f2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3e437078694ca5139d1b",
+    "url": "/monster-rolodex/static/css/main.eff86bfd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3e437078694ca5139d1b",
+    "url": "/monster-rolodex/static/js/main.da05d29a.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
